7809c37731d730a2552273f012fde08b3c324dfe
216037150 <siyabongazungu95@gmail.com>
2024-12-02 23:25:35 +0200

    login, register router link

--- frontend/angular-app/src/app/app-routing.module.ts
+++ frontend/angular-app/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -6,13 +6,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-
 const routes: Routes = [
-  {path: 'login', component: LoginComponent },
-  {path: 'register', component: RegisterComponent },
-  {path: 'about', component:AboutComponent},
-  {path: 'footer', component: FooterComponent},
-  // {path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/home', pathMatch: 'full' } 
 ];
 
 @NgModule({
